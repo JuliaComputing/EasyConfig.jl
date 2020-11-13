f9b475d2d023598e26226c636ef382abbcc45277
joshday <emailjoshday@gmail.com>
2020-11-13 12:23:14 -0500

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,12 @@ using EasyConfig
 using Test
 
 @testset "EasyConfig.jl" begin
-c = config()
+c = Config()
 c.level1.thing = "level1.thing"
 c.level1.level2.a = 1
 c.level1.level2.b = [1,2]
+
+@test c.level1.thing == "level1.thing"
+@test c.level1.level2.a == 1
+@test c.level1.level2.b == [1, 2]
 end
