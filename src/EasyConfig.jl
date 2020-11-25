77d6a9c003ff89bf78e7ce72ada059b086cae2b4
joshday <emailjoshday@gmail.com>
2020-11-25 14:59:04 -0500

    fix propertynames

--- src/EasyConfig.jl
+++ src/EasyConfig.jl
@@ -16,7 +16,7 @@ StructTypes.StructType(::Type{Config}) = StructTypes.DictType()
 
 Base.getproperty(o::Config, k::Symbol) = get!(dict(o), k, Config())
 Base.setproperty!(o::Config, k::Symbol, v) = dict(o)[k] = v
-Base.propertynames(o::Config) = collect(values(dict(o)))
+Base.propertynames(o::Config) = collect(keys(dict(o)))
 
 Base.iterate(o::Config, args...) = iterate(dict(o), args...)
 Base.keys(o::Config) = keys(dict(o))
