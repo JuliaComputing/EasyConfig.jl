8f738519b792ca8ce60a7d61c672827d4ff8ecdc
joshday <emailjoshday@gmail.com>
2021-04-01 10:48:16 -0400

    minor updates, bump version

--- src/EasyConfig.jl
+++ src/EasyConfig.jl
@@ -1,21 +1,47 @@
 module EasyConfig
 
-using OrderedCollections, StructTypes, JSON3
+using OrderedCollections
+using StructTypes
+using JSON3
 
 export Config
 
 #-----------------------------------------------------------------------------# Config
+"""
+    Config(; kw...)
+    Config(::AbstractDict)
+
+A wrapper around an `OrderedDict{Symbol, Any}` with the special properties:
+
+1. You can get/set items via `getproperty`/`setproperty!`:
+
+    c = Config()
+    c.one = 1
+    c.one == 1
+
+2. Properties that don't exist will be created on the fly:
+
+    c = Config()
+    c.one.two.three = "neat!"
+"""
 mutable struct Config <: AbstractDict{Symbol, Any}
     d::OrderedDict{Symbol, Any}
 end
 Config(;kw...) = Config(OrderedDict{Symbol, Any}(kw))
 
+Config(x) = x
+Config(x::AbstractDict) = Config(OrderedDict{Symbol,Any}(Symbol(k) => Config(v) for (k,v) in x))
+
 dict(o::Config) = getfield(o, :d)
 
 StructTypes.StructType(::Type{Config}) = StructTypes.DictType()
 
-Base.getproperty(o::Config, k::Symbol) = get!(dict(o), k, Config())
-Base.setproperty!(o::Config, k::Symbol, v) = dict(o)[k] = v
+Base.getproperty(o::Config, k::Symbol) = get!(o, Symbol(k), Config())
+Base.getproperty(o::Config, k) = getproperty(o, Symbol(k))
+
+Base.setproperty!(o::Config, k::Symbol, v) = dict(o)[Symbol(k)] = v
+Base.setproperty!(o::Config, k, v) = setproperty!(o, Symbol(k), v)
+
 Base.propertynames(o::Config) = collect(keys(dict(o)))
 
 Base.getindex(o::Config, k) = getproperty(o, Symbol(k))
@@ -29,8 +55,6 @@ Base.isempty(o::Config) = isempty(dict(o))
 Base.pairs(o::Config) = pairs(dict(o))
 Base.empty!(o::Config) = (empty!(dict(o)); o)
 Base.get(o::Config, k, default) = get(dict(o), k, default)
-
-Base.show(io::IO, o::Config) = show(io, MIME"text/plain"(), o)
-Base.show(io::IO, ::MIME"text/plain", o::Config) = JSON3.pretty(io, JSON3.write(o))
+Base.get!(o::Config, k, default) = get!(dict(o), k, default)
 
 end # module
