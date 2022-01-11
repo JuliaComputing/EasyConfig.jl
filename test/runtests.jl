ece322eecd9debe62306e10ee84d3bbe917d61ce
joshday <emailjoshday@gmail.com>
2022-01-11 10:27:41 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using EasyConfig
+using StructTypes
+using OrderedCollections: OrderedDict
 using Test
 
 @testset "Constructors" begin
@@ -13,6 +15,7 @@ end
     c.one."two"["three"][:four] = 5
     @test c.one.two.three.four == 5
     c."test" == Config()
+    @test propertynames(c) == [:one, :test]
 end
 
 @testset "Other base methods" begin
@@ -23,18 +26,25 @@ end
 
     @test get!(c, "test2", 1) == 1
     @test c."test2" == 1
+    delete!(c, "test")
+    @test propertynames(c) == [:test2]
+
+    @test keys(Config(x = 1)) == keys(OrderedDict(:x => 1))
+    @test all(values(Config(x = 1)) .== values(OrderedDict(:x => 1)))
+    @test pairs(Config(x = 1)) == pairs(OrderedDict(:x => 1))
+    @test empty!(Config(x=1)) == Config()
 end
 
 @testset "from NamedTuple" begin
     nt = (x=1, y=(x=1, y=(x=1, y=(x=1,y=2))))
     c = Config(nt)
-    c.y.y.y.y == 2
+    @test c.y.y.y.y == 2
 end
 
 @testset "from Dict" begin
     d = Dict(:x => Dict(:x => Dict(:x => Dict(:x => 2))))
     c = Config(d)
-    c.x.x.x.x == 2
+    @test c.x.x.x.x == 2
 end
 
 @testset "merge!" begin
@@ -46,4 +56,17 @@ end
     @test a.x == 5
     @test a.y.x == 5
     @test a.y.z == "hi"
+end
+
+@testset "isempty/delete_empty!" begin
+    c = Config()
+    c.x.x.x
+    @test isempty(c) == true
+end
+
+@testset "StructTypes" begin
+    @test isempty(StructTypes.keyvaluepairs(Config()))
+    c = Config(x=1,y=2)
+    @test StructTypes.keyvaluepairs(c)[:x] == 1
+    @test StructTypes.keyvaluepairs(c)[:y] == 2
 end
